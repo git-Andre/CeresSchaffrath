fcc12280848b08a578dc155041c169747b5a223d
git-Andre <hwcqfK8OGnQX>
2017-05-26 22:44:52 +0200

    footer legal terms... 0.1

--- src/Providers/CeresElectronicRouteServiceProvider.php
+++ src/Providers/CeresElectronicRouteServiceProvider.php
@@ -1,22 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Andre Oelschlaegel
- * Date: 17.05.2017
- * Time: 19:21
- */
+    /**
+     * Created by PhpStorm.
+     * User: Andre Oelschlaegel
+     * Date: 17.05.2017
+     * Time: 19:21
+     */
 
 
-namespace CeresElectronic\Providers;
+    namespace CeresElectronic\Providers;
 
 
-use Plenty\Plugin\RouteServiceProvider;
-use Plenty\Plugin\Routing\Router;
+    use Plenty\Plugin\RouteServiceProvider;
+    use Plenty\Plugin\Routing\Router;
 
-class CeresElectronicRouteServiceProvider extends RouteServiceProvider {
+    class CeresElectronicRouteServiceProvider extends RouteServiceProvider {
 
-    public function map(Router $router)
-    {
-        $router->get('topitems', 'CeresElectronic\Controllers\TopItemsController@showTopItems');
+        public function map(Router $router)
+        {
+            // $router->get('topitems', 'CeresElectronic\Controllers\TopItemsController@showTopItems');
+            $router -> get('agb', function () { return view('Ceres::StaticPages.TermsAndConditions'); });
+        }
     }
-}
