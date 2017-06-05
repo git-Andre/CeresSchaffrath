db57888311ce4208dd2b493fbe300b5d4d5b3f7c
git-Andre <hwcqfK8OGnQX>
2017-06-05 18:46:05 +0200

    routing 1.0 (terms...)

--- src/Providers/CeresSchaffrathServiceProvider.php
+++ src/Providers/CeresSchaffrathServiceProvider.php
@@ -2,43 +2,45 @@
 
     namespace CeresSchaffrath\Providers;
 
-use IO\Extensions\Functions\Partial;
-use IO\Helper\TemplateContainer;
-use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Templates\Twig;
+    use IO\Extensions\Functions\Partial;
+    use IO\Helper\TemplateContainer;
+    use Plenty\Plugin\Events\Dispatcher;
+    use Plenty\Plugin\ServiceProvider;
+    use Plenty\Plugin\Templates\Twig;
 
-class CeresSchaffrathServiceProvider extends ServiceProvider
-{
-    const EVENT_LISTENER_PRIORITY = 90;
+    class CeresSchaffrathServiceProvider extends ServiceProvider {
 
-    /**
-     * Register the service provider.
-     */
+        const EVENT_LISTENER_PRIORITY = 90;
 
-    public function register()
-    {
-        $this -> getApplication() -> register(CeresSchaffrathRouteServiceProvider::class);
+        /**
+         * Register the service provider.
+         */
+
+        public function register()
+        {
+            $this -> getApplication() -> register(CeresSchaffrathRouteServiceProvider::class);
+        }
+
+        public function boot(Twig $twig, Dispatcher $eventDispatcher)
+        {
+
+            // provide template to use for homepage
+            $eventDispatcher -> listen('IO.tpl.home', function (TemplateContainer $container, $templateData) {
+                $container -> setTemplate("CeresSchaffrath::Homepage.Homepage");
+
+                return false;
+            }, self::EVENT_LISTENER_PRIORITY);
+
+
+            $eventDispatcher -> listen('IO.init.templates', function (Partial $partial) {
+                $partial -> set('footer', 'CeresSchaffrath::Footer.ThemeFooter');
+            }, self::EVENT_LISTENER_PRIORITY);
+
+            // provide template to use for single items
+            $eventDispatcher -> listen('IO.tpl.item', function (TemplateContainer $container, $templateData) {
+                $container -> setTemplate("CeresSchaffrath::Item.SingleItem");
+
+                return false;
+            }, self::EVENT_LISTENER_PRIORITY);
+        }
     }
-
-    public function boot(Twig $twig, Dispatcher $eventDispatcher)
-    {
-
-        // provide template to use for homepage
-        $eventDispatcher->listen('IO.tpl.home', function (TemplateContainer $container, $templateData) {
-            $container -> setTemplate("CeresSchaffrath::Homepage.Homepage");
-            return false;
-        }, self::EVENT_LISTENER_PRIORITY);
-
-
-        $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
-            $partial -> set('footer', 'CeresSchaffrath::Footer.ThemeFooter');
-        }, self::EVENT_LISTENER_PRIORITY);
-
-        // provide template to use for single items
-        $eventDispatcher -> listen('IO.tpl.item', function (TemplateContainer $container, $templateData) {
-            $container -> setTemplate("CeresSchaffrath::Item.SingleItem");
-            return false;
-        }, self::EVENT_LISTENER_PRIORITY);
-    }
-}
