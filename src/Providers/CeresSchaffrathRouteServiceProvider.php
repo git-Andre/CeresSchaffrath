7963c09d79207aedcbf43bd404d2ed1746bfea08
git-Andre <post@andre-oelschlaegel.de>
2017-06-07 22:23:58 +0200

    StaticPages 1.12

--- src/Providers/CeresSchaffrathRouteServiceProvider.php
+++ src/Providers/CeresSchaffrathRouteServiceProvider.php
@@ -17,7 +17,7 @@
 
         public function map(Router $router)
         {
-            $router -> get('terms-conditions', 'Ceres::Controllers.StaticPagesController@showTermsAndConditions');
+            $router -> get('terms-conditions', 'CeresSchaffrath::Controllers.StaticPagesController@showTermsAndConditions');
 
             $router -> get('versandkosten', 'CeresSchaffrath::Controllers.StaticPagesController@showVersandkosten');
 
