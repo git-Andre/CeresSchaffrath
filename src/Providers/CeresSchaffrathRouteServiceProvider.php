81b6d024054b225277f6ed453fce0c2f7cb490d6
git-Andre <post@andre-oelschlaegel.de>
2017-06-22 18:47:40 +0200

    Routes nach Ceres 1.03

--- src/Providers/CeresSchaffrathRouteServiceProvider.php
+++ src/Providers/CeresSchaffrathRouteServiceProvider.php
@@ -16,21 +16,21 @@
          */
         public function map(Router $router)
         {
-            $router -> get('versandkosten', 'CeresSchaffrath\StaticPagesRoutesController@showVersandkosten');
-            $router -> get('zahlungsarten', 'CeresSchaffrath\StaticPagesRoutesController@showZahlungsarten');
-            $router -> get('kontakt', 'CeresSchaffrath\StaticPagesRoutesController@showKontakt');
-            $router -> get('hilfe', 'CeresSchaffrath\StaticPagesRoutesController@showHilfe');
+            $router -> get('versandkosten', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showVersandkosten');
+            $router -> get('zahlungsarten', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showZahlungsarten');
+            $router -> get('kontakt', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showKontakt');
+            $router -> get('hilfe', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showHilfe');
 
-            $router -> get('impressum', 'CeresSchaffrath\StaticPagesRoutesController@showImpressum');
-            $router -> get('legal-disclosure', 'CeresSchaffrath\StaticPagesRoutesController@showImpressum');
+            $router -> get('impressum', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showImpressum');
+            $router -> get('legal-disclosure', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showImpressum');
 
-            $router -> get('widerruf', 'CeresSchaffrath\StaticPagesRoutesController@showWiderruf');
-            $router -> get('cancellation-rights', 'CeresSchaffrath\StaticPagesRoutesController@showWiderruf');
+            $router -> get('widerruf', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showWiderruf');
+            $router -> get('cancellation-rights', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showWiderruf');
 
-            $router -> get('agb', 'CeresSchaffrath\StaticPagesRoutesController@showAgb');
-            $router -> get('terms-conditions', 'CeresSchaffrath\StaticPagesRoutesController@showAgb');
+            $router -> get('agb', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showAgb');
+            $router -> get('terms-conditions', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showAgb');
 
-            $router -> get('datenschutz', 'CeresSchaffrath\StaticPagesRoutesController@showDatenschutz');
-            $router -> get('privacy-policy', 'CeresSchaffrath\StaticPagesRoutesController@showDatenschutz');
+            $router -> get('datenschutz', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showDatenschutz');
+            $router -> get('privacy-policy', 'CeresSchaffrath/Controllers/StaticPagesRoutesController@showDatenschutz');
         }
     }
