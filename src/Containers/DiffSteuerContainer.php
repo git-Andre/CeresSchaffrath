9acb2b934fac8deb97d77163567ce2a64d9baef2
git-Andre <post@andre-oelschlaegel.de>
2017-06-25 22:04:02 +0200

    Single Item nach CeresSchaffrath 1.5 Test Container mit Object

--- src/Containers/DiffSteuerContainer.php
+++ src/Containers/DiffSteuerContainer.php
@@ -4,6 +4,37 @@
 
     use Plenty\Plugin\Templates\Twig;
 
+    // use IO\Builder\Item\Fields\ItemCrossSellingFields;
+    // use IO\Builder\Item\Fields\ItemDescriptionFields;
+    // use IO\Builder\Item\Fields\VariationAttributeValueFields;
+    // use IO\Builder\Item\Fields\VariationBaseFields;
+    // use IO\Builder\Item\ItemColumnBuilder;
+    // use IO\Builder\Item\ItemFilterBuilder;
+    // use IO\Builder\Item\ItemParamsBuilder;
+    // use IO\Builder\Item\Params\ItemColumnsParams;
+    // use IO\Constants\CrossSellingType;
+    // use IO\Constants\ItemConditionTexts;
+    // use IO\Constants\Language;
+    // use Plenty\Modules\Cloud\ElasticSearch\Lib\ElasticSearch;
+    // use Plenty\Modules\Cloud\ElasticSearch\Lib\Processor\DocumentProcessor;
+    // use Plenty\Modules\Cloud\ElasticSearch\Lib\Search\Document\DocumentSearch;
+    // use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\IncludeSource;
+    // use Plenty\Modules\Item\Attribute\Contracts\AttributeNameRepositoryContract;
+    // use Plenty\Modules\Item\Attribute\Contracts\AttributeValueNameRepositoryContract;
+    // use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
+    // use Plenty\Modules\Item\DataLayer\Models\Record;
+    // use Plenty\Modules\Item\DataLayer\Models\RecordList;
+    // use Plenty\Modules\Item\Search\Aggregations\AttributeValueListAggregation;
+    // use Plenty\Modules\Item\Search\Aggregations\AttributeValueListAggregationProcessor;
+    // use Plenty\Modules\Item\Search\Contracts\VariationElasticSearchSearchRepositoryContract;
+    // use Plenty\Modules\Item\Search\Filter\CategoryFilter;
+    // use Plenty\Modules\Item\Search\Filter\ClientFilter;
+    // use Plenty\Modules\Item\Search\Filter\SearchFilter;
+    // use Plenty\Modules\Item\Search\Filter\VariationBaseFilter;
+    // use Plenty\Plugin\Application;
+
+
+
     class DiffSteuerContainer {
 
         public function call(Twig $twig, $itemData) : string
