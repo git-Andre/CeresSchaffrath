1bb73fa7a759f71fc3998b72cc9725da194f508a
git-Andre <post@andre-oelschlaegel.de>
2017-06-30 12:24:09 +0200

    Category nach Schaffrath 1.2

--- src/Providers/CeresSchaffrathServiceProvider.php
+++ src/Providers/CeresSchaffrathServiceProvider.php
@@ -40,15 +40,22 @@
                 return false;
             }, self::EVENT_LISTENER_PRIORITY);
 
+
+            $eventDispatcher -> listen('IO.init.templates', function (Partial $partial) {
+                $partial -> set('footer', 'CeresSchaffrath::Footer.ThemeFooter');
+            }, self::EVENT_LISTENER_PRIORITY);
+
 // provide template to use for single items
             $eventDispatcher -> listen('IO.tpl.item', function (TemplateContainer $container, $templateData) {
                 $container -> setTemplate("CeresSchaffrath::Item.SingleItem");
                 return false;
             }, self::EVENT_LISTENER_PRIORITY);
 
-// Footer
-            $eventDispatcher -> listen('IO.init.templates', function (Partial $partial) {
-                $partial -> set('footer', 'CeresSchaffrath::Footer.ThemeFooter');
+// provide template to use for Category items
+            $eventDispatcher -> listen('IO.category.item', function (TemplateContainer $container, $templateData) {
+                $container -> setTemplate("CeresSchaffrath::Category.Item.CategoryItem");
+
+                return false;
             }, self::EVENT_LISTENER_PRIORITY);
 
         }
