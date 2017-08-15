140c4488cf7823b364407826382e27c31d0e2170
git-Andre <post@andre-oelschlaegel.de>
2017-08-15 22:31:47 +0200

    Ceres nochmal Start Auktion 2 test

---
+++ src/Services/AuctionService.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: andreoelschlaegel
+     * Date: 15.08.17
+     * Time: 17:17
+     */
+
+    namespace CeresSchaffrath\Services;
+
+use PluginAuctions\Services\Database\AuctionsService;
+
+    class AuctionService {
+
+    }
