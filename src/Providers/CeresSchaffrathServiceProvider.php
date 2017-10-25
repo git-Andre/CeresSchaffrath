3d9707f877e9bc874ca6e3c8d4a954d716a0a130
git-Andre <post@andre-oelschlaegel.de>
2017-10-25 17:39:45 +0200

    Category Übernahme 99.99 22

--- src/Providers/CeresSchaffrathServiceProvider.php
+++ src/Providers/CeresSchaffrathServiceProvider.php
@@ -47,7 +47,7 @@
             }, self::EVENT_LISTENER_PRIORITY);
 
 // provide template to use for Category items
-             $eventDispatcher -> listen('IO.category.item', function (TemplateContainer $container, $templateData) {
+             $eventDispatcher -> listen('IO.tpl.category.item', function (TemplateContainer $container, $templateData) {
                  $container -> setTemplate("CeresSchaffrath::Category.Item.CategoryItem");
 
                  return false;
