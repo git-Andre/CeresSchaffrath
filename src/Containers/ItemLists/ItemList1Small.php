0c407d3b14dea154701a425ac50fe897390efbff
git-Andre <post@andre-oelschlaegel.de>
2017-10-28 13:27:54 +0200

    homepage last item teaser + small Container3

--- src/Containers/ItemLists/ItemList1Small.php
+++ src/Containers/ItemLists/ItemList1Small.php
@@ -4,9 +4,8 @@
 
     use Plenty\Plugin\Templates\Twig;
 
-    class ItemLists1Small
+    class ItemList1Small
     {
-
         public function call(Twig $twig) : string
         {
             return $twig -> render('CeresSchaffrath::Containers.ItemLists.ItemList1Small');
